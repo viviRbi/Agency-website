e1ec8ab628321dea0d8f189f450ba20c8332017e
viviRbi <50475727+viviRbi@users.noreply.github.com>
2019-10-21 17:52:29 -0500

    update

--- script/script.js
+++ script/script.js
@@ -3,5 +3,39 @@
 ###################################*/
 $(window).on("load", function(){ //when site fully loaded
 	$("#status").fadeOut();
-	$("#preloader").delay(350).fadeOut();
+	$("#preloader").delay(350).fadeOut("slow");
+});
+
+/* #################################
+		Team
+###################################*/
+$(function(){
+	$("#team-members").owlCarousel({
+		items: 2,
+		autoplay: true,
+		smartSpeed: 700,
+		loop: true,
+		autoplayHoverPause: true,
+		nav: true,
+		navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
+	});
+});
+/* ----------------------
+		Progress Bar
+-------------------------*/
+$(function(){
+
+	$("#progress-elements").waypoint(function(){
+
+		$(".progress-bar").each(function(){
+			$(this).animate({
+				width: $(this).attr("aria-valuenow") + "%"
+			}, 1000);
+		});
+		this.destroy();
+
+	}, {offset: "bottom-in-view"
+	});
+
+
 });
