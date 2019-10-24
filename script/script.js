02eb97325916c3aea96f8c49674b65ef2fe8a6de
viviRbi <50475727+viviRbi@users.noreply.github.com>
2019-10-23 23:13:22 -0500

    Pricing section

--- script/script.js
+++ script/script.js
@@ -86,3 +86,17 @@ $(function(){
 			}
 		});
 	});
+	/*------------------------------
+		Testimonials
+------------------------------*/
+$(function(){
+	$("#testimonial-slider").owlCarousel({
+		items: 1,
+		autoplay: false,
+		smartSpeed: 700,
+		loop: true,
+		autoplayHoverPause: true,
+		nav: true,
+		navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
+	});
+});
