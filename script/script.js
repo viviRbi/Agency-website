819204fb01609ae2543e0b2f4e1588e43c25da0b
viviRbi <50475727+viviRbi@users.noreply.github.com>
2019-10-30 22:12:00 -0500

    footer, nav bar, and scrolling effect

--- script/script.js
+++ script/script.js
@@ -114,42 +114,99 @@ $ (function(){
 	/*------------------------------
 		Clients
 ------------------------------*/
-$(function(){
-	$("#clients-list").owlCarousel({
-		items: 6,
-		autoplay: false,
-		smartSpeed: 700,
-		loop: true,
-		autoplayHoverPause: true,
-		nav: true,
-		dots: false,
-		navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>']
+	$(function(){
+		$("#clients-list").owlCarousel({
+			items: 6,
+			autoplay: false,
+			smartSpeed: 700,
+			loop: true,
+			autoplayHoverPause: true,
+			nav: true,
+			dots: false,
+			navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>']
+		});
 	});
-});
 /*-------------------------
 		Google Map
 --------------------------*/
-$(window).on('load', function(){
-	//map var
-	var addressString = '230 Broadway, NY, New York 10007, USA';
-	var myLatlng={lat: 40.712685, lng: -74.005920};
+	$(window).on('load', function(){
+		//map var
+		var addressString = '230 Broadway, NY, New York 10007, USA';
+		var myLatlng={lat: 40.712685, lng: -74.005920};
 
-	//1. Render google map
-	var map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 11,
-		center: myLatlng
+		//1. Render google map
+		var map = new google.maps.Map(document.getElementById('map'), {
+			zoom: 11,
+			center: myLatlng
+		});
+		//2. Add marker
+		var marker = new google.maps.Marker({
+			position: myLatlng,
+			map: map,
+			title: "Click To See Address"
+		})
+		//3.Info window
+		var infowindow = new google.maps.InfoWindow({
+			content: addressString
+		});
+		marker.addListener('click', function(){
+			infowindow.open(map, marker);
+		});
 	});
-	//2. Add marker
-	var marker = new google.maps.Marker({
-		position: myLatlng,
-		map: map,
-		title: "Click To See Address"
-	})
-	//3.Info window
-	var infowindow = new google.maps.InfoWindow({
-		content: addressString
+/*-------------------------
+		Navigation
+--------------------------*/
+
+	/*Show/hide white navigation*/
+	$(function(){
+		$(window).scroll(function(){
+			if( $(window).scrollTop() > 50){
+
+				$('nav').addClass("white-nav-top");
+
+				//dard logo
+				$(".navbar-brand img").attr('src','img/logo/logo-dark.png');
+
+				//Show back to top btn
+				$("#back-to-top").fadeIn();
+
+			}else{
+				$('nav').removeClass("white-nav-top");
+
+				//white logo
+				$(".navbar-brand img").attr('src','img/logo/logo.png');
+
+				//Hide back to top btn
+				$("#back-to-top").fadeOut();
+			}
+		});
 	});
-	marker.addListener('click', function(){
-		infowindow.open(map, marker);
+	//smooth scroll
+	$(function (){
+		$('a.smooth-scroll').click(function(event){
+			event.preventDefault();
+			//get section Id like #about, #work, etc
+			var section_id = $(this).attr("href");
+
+			$("html, body").animate({
+				scrollTop: $(section_id).offset().top
+			},1250, "easeInOutExpo");
+
+		});
 	});
+
+
+// t: current time, b: begInnIng value, c: change In value, d: duration
+jQuery.easing['jswing'] = jQuery.easing['swing'];
+
+jQuery.extend( jQuery.easing,
+{
+	def: 'easeOutQuad',
+	
+	easeInOutExpo: function (x, t, b, c, d) {
+		if (t==0) return b;
+		if (t==d) return b+c;
+		if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
+		return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
+	}	
 });
